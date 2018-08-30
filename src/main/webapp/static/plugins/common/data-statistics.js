c61be77a9710f24dfe306a1c52e816d4312b9e96
sjh0824 <1204563557@qq.com>
2018-08-30 11:39:05 +0800

    界面样式修改

--- src/main/webapp/static/plugins/common/data-statistics.js
+++ src/main/webapp/static/plugins/common/data-statistics.js
@@ -1,28 +1,21 @@
 /**
  * Created by 12045 on 2018/7/4.
  */
-var wholeAppInfoId,wholeStartTime,wholeEndTime;
+var wholeAppInfoId, wholeStartTime, wholeEndTime;
 (function () {
 
 // 获取用户所有小程序
-    allSmallProgram();
+//     allSmallProgram();
     // 自定义时间选择
     laydateTime();
 
-    selecyCondition()
+    // selecyCondition()
 
     dateSelecteTime()
 
-    // coreDataSel()
-    nestingPie()
-    lineBarData()
-    barData()
-    radarData()
-
-    // coreDataShow()
-    // $("#navbarH").height($("#page-wrapper").height() - 80)
 
 
+    $($("#contain_main_head > div")[2]).find('input').trigger("click");
 }())
 
 // 获取用户所有小程序
@@ -32,12 +25,12 @@ function allSmallProgram() {
         dataType: "json",
         success: function (data) {
 
-            if(data.success){
+            if (data.success) {
                 var arrData = data.wechatAppInfoEntityList;
-                var html='';
-                for(var i=0;i<arrData.length;i++){
+                var html = '';
+                for (var i = 0; i < arrData.length; i++) {
 
-                    html += '<li data-appInfoId="'+ arrData[i].appInfoId+'" data-appid="'+ arrData[i].appid +'" data-secret="'+ arrData[i].secret +'"><a href="javascript:void(0)">'+arrData[i].appName+ '</a></li>';
+                    html += '<li data-appInfoId="' + arrData[i].appInfoId + '" data-appid="' + arrData[i].appid + '" data-secret="' + arrData[i].secret + '"><a href="javascript:void(0)">' + arrData[i].appName + '</a></li>';
                 }
                 $("#contain_head_left_ul").html(html);
 
@@ -58,7 +51,7 @@ function laydateTime() {
         , done: function (value, date) {
             $(".contain_main_title .time1").html(value)
             wholeStartTime = value;
-            if(!wholeEndTime){
+            if (!wholeEndTime) {
                 wholeEndTime = value;
             }
             changeTimeAfterDataChange()
@@ -72,7 +65,7 @@ function laydateTime() {
         , value: time
         , done: function (value, date) {
             $(".contain_main_title .time2").html(value)
-            if(!wholeStartTime){
+            if (!wholeStartTime) {
                 wholeStartTime = value;
             }
             wholeEndTime = value
@@ -83,7 +76,7 @@ function laydateTime() {
 
 // 筛选条件的选择
 function selecyCondition() {
-    $("#user_manage .dropdown-menu").off('click',"li");
+    $("#user_manage .dropdown-menu").off('click', "li");
     $("#user_manage .dropdown-menu").on('click', 'li', function () {
 
         var that = this;
@@ -94,14 +87,13 @@ function selecyCondition() {
             case "dropdownMenu1":
                 $(target).html(content)
                 var appInfoId = $(that).attr("data-appInfoId");
-                if(appInfoId==1){
+                if (appInfoId == 1) {
                     wholeAppInfoId = "5b3dd7c91d76102dd8a2d0d4"
-                }else{
+                } else {
                     wholeAppInfoId = appInfoId
                 }
 
                 // changeTimeAfterDataChange();
-                $($("#contain_main_head > div")[2]).find('input').trigger("click");
                 break;
             case "dropdownMenu2":
                 $(target).html(content)
@@ -171,53 +163,109 @@ function currentTime(myDate) {
 
 // 改变时间，数据及图表相继改变
 function changeTimeAfterDataChange() {
-    // newAndOldUsers(wholeAppInfoId)
-    // nestingPie()
+    gainPieData();
+    gainLineData();
+    // gainBarData();
+    gainRadarData();
 }
 
+// 获取 嵌套环形图 数据
+function gainPieData() {
+    $.ajax({
+        url: "/cto/total/totalCtoCatgView",
+        data: {startDate: wholeStartTime, endDate: wholeEndTime},
+        dataType: "json",
+        success: function (data) {
+            if (data.success) {
+                nestingPie(data.data)
+            }
+        }
+    })
+}
+
+// 获取 条形图 数据
+function gainLineData() {
+    $.ajax({
+        url: "/cto/total/totalHotTopTenView",
+        data: {startDate: wholeStartTime, endDate: wholeEndTime},
+        dataType: "json",
+        success: function (data) {
+            if (data.success) {
+                lineBarData(data.data)
+            }
+        }
+    })
+}
+
+// 获取 柱状图 数据
+function gainBarData() {
+    $.ajax({
+        url: "/cto/total/totalChangePartsView",
+        data: {startDate: wholeStartTime, endDate: wholeEndTime},
+        dataType: "json",
+        success: function (data) {
+            if (data.success) {
+                barData(data.data)
+            }
+        }
+    })
+}
+
+// 获取 雷达图 数据
+function gainRadarData() {
+    $.ajax({
+        url: "/cto/total/totalHotPricesView",
+        data: {startDate: wholeStartTime, endDate: wholeEndTime},
+        dataType: "json",
+        success: function (data) {
+            if (data.success) {
+                radarData(data.data)
+            }
+        }
+    })
+}
+
+
+
 // 最受欢迎产品系列——嵌套环形图
-function nestingPie() {
+function nestingPie(data) {
+    var largeData = [], lowerData = []
+    for (var i = 0; i < data.length; i++) {
+        largeData.push({name: data[i].name, value: data[i].number});
+        if (data[i].child && data[i].child.length > 0) {
+            var childData = data[i].child;
+            for (var j = 0; j < childData.length; j++) {
+                lowerData.push({name: childData[j].name, value: childData[j].number})
+            }
+        }
+    }
     var myChartPie = echarts.init(document.getElementById('main_pie'));
     var option = {
         tooltip: {
             trigger: 'item',
             formatter: "{a} <br/>{b}: {c} ({d}%)",
-            // position:function(p){ //其中p为当前鼠标的位置
-            //     return [p[0] + 10, p[1] - 10];
-            // },
-            // extraCssText:'width:180px;height:55px;'
-        },
-        legend: {
-            orient: 'vertical',
-            x: 'left',
-            data:['直达','营销广告','搜索引擎','邮件营销','联盟广告','视频广告','百度','谷歌','必应','其他']
         },
         series: [
             {
-                name:'访问来源',
-                type:'pie',
+                name: '电脑类型',
+                type: 'pie',
                 selectedMode: 'single',
                 radius: [0, '30%'],
 
                 label: {
-                    normal: {
-                        position: 'inner'
-                    }
+                    position: 'center',
+                    color:"#fff"
                 },
                 labelLine: {
                     normal: {
                         show: false
                     }
                 },
-                data:[
-                    {value:335, name:'直达', selected:true},
-                    {value:679, name:'营销广告'},
-                    {value:1548, name:'搜索引擎'}
-                ]
+                data: largeData
             },
             {
-                name:'访问来源',
-                type:'pie',
+                name: '系列',
+                type: 'pie',
                 radius: ['40%', '55%'],
                 label: {
                     normal: {
@@ -232,7 +280,6 @@ function nestingPie() {
                                 lineHeight: 22,
                                 align: 'center'
                             },
-
                             hr: {
                                 borderColor: '#aaa',
                                 width: '100%',
@@ -252,16 +299,7 @@ function nestingPie() {
                         }
                     }
                 },
-                data:[
-                    {value:335, name:'直达'},
-                    {value:310, name:'邮件营销'},
-                    {value:234, name:'联盟广告'},
-                    {value:135, name:'视频广告'},
-                    {value:1048, name:'百度'},
-                    {value:251, name:'谷歌'},
-                    {value:147, name:'必应'},
-                    {value:102, name:'其他'}
-                ]
+                data: lowerData
             }
         ]
     };
@@ -272,26 +310,27 @@ function nestingPie() {
 }
 
 // 最受欢迎机型——条形图
-function lineBarData() {
+function lineBarData(data) {
+    var nameData = [],setData=[];
+    data = data.sort(compare("totalNum"))
+    console.log(data)
+    for (var i = 0; i < data.length; i++) {
+        nameData.push(data[i].showText)
+        setData.push(data[i].totalNum)
+    }
+
     var myChartPie = echarts.init(document.getElementById('main_line'));
 
     var option = {
-        title: {
-            text: '世界人口总量',
-            subtext: '数据来自网络'
-        },
+        // title: {
+        //     text: '世界人口总量',
+        //     subtext: '数据来自网络'
+        // },
         tooltip: {
             trigger: 'axis',
             axisPointer: {
                 type: 'shadow'
-            },
-            // position:function(p){ //其中p为当前鼠标的位置
-            //     return [p[0] + 10, p[1] - 10];
-            // },
-            // extraCssText:'width:180px;height:55px;'
-        },
-        legend: {
-            data: [ '2012年']
+            }
         },
         grid: {
             left: '3%',
@@ -305,13 +344,14 @@ function lineBarData() {
         },
         yAxis: {
             type: 'category',
-            data: ['巴西','印尼','美国','印度','中国','世界人口(万)']
+            data:nameData
         },
         series: [
             {
                 name: '2012年',
                 type: 'bar',
-                data: [19325, 23438, 31000, 121594, 134141, 681807]
+                barMaxWidth: "50px",
+                data: setData
             }
         ]
     };
@@ -324,15 +364,16 @@ function lineBarData() {
 }
 
 // 高频定制需求分析——柱状图
-function barData() {
+function barData(data) {
+    console.log(data)
     var myChartPie = echarts.init(document.getElementById('main_bar'));
 
     var option = {
         color: ['#3398DB'],
-        tooltip : {
+        tooltip: {
             trigger: 'axis',
-            axisPointer : {            // 坐标轴指示器，坐标轴触发有效
-                type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+            axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+                type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
             },
             // position:function(p){ //其中p为当前鼠标的位置
             //     return [p[0] + 10, p[1] - 10];
@@ -345,26 +386,26 @@ function barData() {
             bottom: '3%',
             containLabel: true
         },
-        xAxis : [
+        xAxis: [
             {
-                type : 'category',
-                data : ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+                type: 'category',
+                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
                 axisTick: {
                     alignWithLabel: true
                 }
             }
         ],
-        yAxis : [
+        yAxis: [
             {
-                type : 'value'
+                type: 'value'
             }
         ],
-        series : [
+        series: [
             {
-                name:'直接访问',
-                type:'bar',
-                barWidth: '60%',
-                data:[10, 52, 200, 334, 390, 330, 220]
+                name: '直接访问',
+                type: 'bar',
+                barWidth: '50px',
+                data: [10, 52, 200, 334, 390, 330, 220]
             }
         ]
     };
@@ -375,25 +416,27 @@ function barData() {
     }
 
 }
+
 // 最受欢迎价格区间——雷达图
-function radarData() {
+function radarData(data) {
+    console.log(data)
+    var nameData = [], setData=[]
+    for(index in data){
+        console.log(index)
+        nameData.push({name:index})
+        setData.push(data[index])
+        console.log(data[index])
+    }
+
     var myChartPie = echarts.init(document.getElementById('main_radar'));
 
     var option = {
-        title: {
-            text: '基础雷达图'
-        },
-        tooltip: {
-            // position:function(p){ //其中p为当前鼠标的位置
-            //     return [p[0] + 10, p[1] - 10];
-            // },
-            // extraCssText:'width:340px;height:170px;'
-        },
-        // legend: {
-        //     data: [ '实际开销（Actual Spending）']
+        // title: {
+        //     text: '基础雷达图'
         // },
+        tooltip: {
+        },
         radar: {
-            // shape: 'circle',
             name: {
                 textStyle: {
                     color: '#fff',
@@ -402,23 +445,15 @@ function radarData() {
                     padding: [3, 5]
                 }
             },
-            indicator: [
-                { name: '销售（sales）', max: 6500},
-                { name: '管理（Administration）', max: 16000},
-                { name: '信息技术（Information Techology）', max: 30000},
-                { name: '客服（Customer Support）', max: 38000},
-                { name: '研发（Development）', max: 52000},
-                { name: '市场（Marketing）', max: 25000}
-            ]
+            indicator: nameData
         },
         series: [{
             name: '预算 vs 开销（Budget vs spending）',
             type: 'radar',
-            // areaStyle: {normal: {}},
-            data : [
+            data: [
                 {
-                    value : [5000, 14000, 28000, 31000, 42000, 21000],
-                    name : '实际开销（Actual Spending）'
+                    value: setData,
+                    name: '实际开销（Actual Spending）'
                 }
             ]
         }]
@@ -427,6 +462,15 @@ function radarData() {
     myChartPie.setOption(option);
     window.onresize = function () {
         myChartPie.resize();
+    }
+}
+
+
+function compare(property){
+    return function(a,b){
+        var value1 = a[property];
+        var value2 = b[property];
+        return value1 - value2;
     }
 }
 
