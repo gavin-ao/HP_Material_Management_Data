9a15ef93a008995075c39c9691c359ed1c0f4380
1670599538 <1670599538@qq.com>
2018-08-31 11:20:56 +0800

    1、添加手机号码 2、添加轮播图 findProDuctCatgList 方法中

--- src/main/java/data/driven/cto/business/wechat/WechatAppInfoService.java
+++ src/main/java/data/driven/cto/business/wechat/WechatAppInfoService.java
@@ -2,6 +2,8 @@ package data.driven.cto.business.wechat;
 
 import data.driven.cto.entity.wechat.WechatAppInfoEntity;
 
+import java.util.List;
+
 /**
  * 小程序信息service
  * @author hejinkai
@@ -15,5 +17,19 @@ public interface WechatAppInfoService {
      * @return
      */
     public WechatAppInfoEntity getAppInfo(String appid);
+
+    /**
+     * 根据微信小程序查询轮播图
+     * @param appInfoId
+     * @return
+     */
+    public List<String> findSowingMap(String appInfoId);
+
+    /**
+     * 根据主键获取手机号码-联系方式
+     * @param appInfoId
+     * @return
+     */
+    public String getMobilePhone(String appInfoId);
 
 }
--- src/main/java/data/driven/cto/business/wechat/impl/WechatAppInfoServiceImpl.java
+++ src/main/java/data/driven/cto/business/wechat/impl/WechatAppInfoServiceImpl.java
@@ -20,7 +20,7 @@ public class WechatAppInfoServiceImpl implements WechatAppInfoService {
 
     @Override
     public WechatAppInfoEntity getAppInfo(String appid) {
-        String sql = "select app_info_id,app_name,appid,secret,create_at,creator from wechat_app_info where appid = ?";
+        String sql = "select app_info_id,app_name,appid,secret,mobile_phone,create_at,creator from wechat_app_info where appid = ?";
         List<WechatAppInfoEntity> list = jdbcBaseDao.queryList(WechatAppInfoEntity.class, sql, appid);
         if(list != null && list.size() > 0){
             return list.get(0);
@@ -28,4 +28,21 @@ public class WechatAppInfoServiceImpl implements WechatAppInfoService {
         return null;
     }
 
+    @Override
+    public List<String> findSowingMap(String appInfoId) {
+        String sql = "select p.file_path from wechat_app_sowing_map sm" +
+                " left join sys_picture p on p.picture_id = sm.picture_id where sm.app_info_id = ? order by sm.ord";
+        List<String> filePathList = jdbcBaseDao.getColumns(String.class, sql, appInfoId);
+        return filePathList;
+    }
+
+    @Override
+    public String getMobilePhone(String appInfoId) {
+        String sql = "select mobile_phone from wechat_app_info where app_info_id = ?";
+        Object mobilePhone = jdbcBaseDao.getColumn(sql, appInfoId);
+        if(mobilePhone != null){
+            return mobilePhone.toString();
+        }
+        return null;
+    }
 }
